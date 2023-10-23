7614f5c588094e85d4187856361976bb5c45a938
Binti Bhatt <bintijbhatt@gmail.com>
2023-10-23 23:11:50 +0530

    First Commit - beginning

---
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../Css/Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer">
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
